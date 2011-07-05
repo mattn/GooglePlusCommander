98a06697bedb311e202c78c268d99e39e34a22bb
mattn <mattn.jp@gmail.com>
2011-07-05 15:49:07 +0900

    retab.

--- googlepluscommander.user.js
+++ googlepluscommander.user.js
@@ -8,50 +8,49 @@
 // ==/UserScript==
 
 (function() {
-	function click(elem) {
-		var e = document.createEvent("MouseEvent");
-		e.initMouseEvent("click", true, true, event.view, 0, 0, 0, 0, 0, false, false, false, 0, null);
-		elem.dispatchEvent(e);
-	}
+  function click(elem) {
+    var e = document.createEvent("MouseEvent");
+    e.initMouseEvent("click", true, true, event.view, 0, 0, 0, 0, 0, false, false, false, 0, null);
+    elem.dispatchEvent(e);
+  }
 
-	function plus(elem) {
-		var elems = elem.getElementsByTagName('button');
-		for (var n = 0; n < elems.length; n++) {
-			if (elems[n].getAttribute('g:type') == 'plusone') {
-				click(elems[n]);
-			}
-		}
-	}
+  function plus(elem) {
+    var elems = elem.getElementsByTagName('button');
+    for (var n = 0; n < elems.length; n++) {
+      if (elems[n].getAttribute('g:type') == 'plusone') {
+        click(elems[n]);
+      }
+    }
+  }
 
-	function tools(elem) {
-		return Array.prototype.slice.call(elem.getElementsByTagName('span'), 0).filter(function(e, i, a) {
-			if (e.getAttribute('role') != 'button') return false;
-			return true;
-		});
-	}
+  function tools(elem) {
+    return Array.prototype.slice.call(elem.getElementsByTagName('span'), 0).filter(function(e, i, a) {
+      if (e.getAttribute('role') != 'button') return false;
+      return true;
+    });
+  }
 
-	function install(elem) {
-		elem.addEventListener('keyup', function(e) {
-			switch (e.keyCode) {
-				case 67:
-					elems = tools(e.target);
-					click(elems[elems.length - 2]);
-					break;
-				case 83:
-					elems = tools(e.target);
-					click(elems[elems.length - 1]);
-					break;
-				case 187:
-					plus(e.target);
-					break;
-			}
-		}, false)
-	}
+  function install(elem) {
+    elem.addEventListener('keyup', function(e) {
+      switch (e.keyCode) {
+        case 67:
+          elems = tools(e.target);
+          click(elems[elems.length - 2]);
+          break;
+        case 83:
+          elems = tools(e.target);
+          click(elems[elems.length - 1]);
+          break;
+        case 187:
+          plus(e.target);
+          break;
+      }
+    }, false)
+  }
 
-	var elems = document.getElementsByTagName('div');
-	for (var n = 0; n < elems.length; n++) {
-		var e = elems[n];
-		if (e.id.substring(0, 7) == 'update-') install(e);
-	}
+  var elems = document.getElementsByTagName('div');
+  for (var n = 0; n < elems.length; n++) {
+    var e = elems[n];
+    if (e.id.substring(0, 7) == 'update-') install(e);
+  }
 })()
-
